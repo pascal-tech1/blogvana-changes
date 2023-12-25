75f376ba700ceba56eac49af225650a50d0eb0e9
Pascal <pascalazubike003@@@gmail.com>
2023-12-25 18:02:46 +0100

    added table of content for single page for user easy navigation

--- backend/config/db/dbConnect.js
+++ backend/config/db/dbConnect.js
@@ -1,31 +1,31 @@
 const mongoose = require("mongoose");
 
-// const connectDB = async () => {
-// 	try {
-// 		const conn = await mongoose.connect(process.env.MONGODB_URI);
-// 		console.log(`Mongo db connected: ${conn.connection.host}`);
-// 	} catch (error) {
-// 		console.log(error);
-// 		process.exit(1);
-// 	}
-// };
-
-// module.exports = connectDB;
-
-const url = "mongodb://127.0.0.1:27017/mern-blog-app";
-
-mongoose.connect(url, { useNewUrlParser: true });
-
-const db = mongoose.connection;
-
-const connectDB = () => {
-	db.once("open", (_) => {
-		console.log("Database connected:", url);
-	});
-
-	db.on("error", (err) => {
-		console.error("connection error:", err);
-	});
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGODB_URI);
+		console.log(`Mongo db connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
+
+// const url = "mongodb://127.0.0.1:27017/mern-blog-app";
+
+// mongoose.connect(url, { useNewUrlParser: true });
+
+// const db = mongoose.connection;
+
+// const connectDB = () => {
+// 	db.once("open", (_) => {
+// 		console.log("Database connected:", url);
+// 	});
+
+// 	db.on("error", (err) => {
+// 		console.error("connection error:", err);
+// 	});
+// };
+
+// module.exports = connectDB;
