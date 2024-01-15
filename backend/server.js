df1614a3d91bcfe13b61af73885ed4f5d5bc34e8
Pascal <pascalazubike003@@@gmail.com>
2024-01-16 00:33:23 +0100

    change postinfo and likes and dislikes ui

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,42 @@ const {
 	NotFoundErrorhandler,
 } = require("./middlewares/error/errorhandler");
 const messageRoutes = require("./route/message/messageRoutes");
-// const PlayHT = require("playht");
+const PlayHT = require("playht");
 
-// PlayHT.init({
-// 	apiKey: "<YOUR API KEY>",
-// 	userId: "<YOUR API KEY>",
-// });
+PlayHT.init({
+	apiKey: "8d87d023e0f84fa681b6c717594d3375",
+	userId: "EoJx53yPDBOTD5x2SiGr9b14hqI3",
+	defaultVoiceId:
+		"s3://peregrine-voices/oliver_narrative2_parrot_saad/manifest.json",
+	defaultVoiceEngine: "PlayHT2.0",
+});
+
+// // Generate audio from text
+// (async function () {
+// 	const text = [
+// 		`In the context of organizations, high psychological safety drives performance and innovation, while its absence leads to reduced productivity and increased burnout. Google’s Project Aristotle demonstrated that IQ and financial resources are not always synonymous with effective outcomes. In fact, the study identified psychological safety as the most crucial factor in `,
+// 		`    explaining high performance. — Excerpt from the book “The 4 Stages of Psychological Safety”.
+
+// 	The subtitle of the book is “Defining the Path to Inclusion and Innovation”, and in it, the author compellingly argues that innovation is limited in environments lacking psychological safety. Even if innovation is not a direct goal for you or your company, this discussion offers a renewed`,
+// 		`perspective on people and organizations. I was grateful and surprised by the opportunity to explore this theme more deeply, the most impactful aspects of which I share in this article.
+// 	Respect, Permission, and Psychological Safety
+	
+// 	In the image above, observe two fundamental variables on the gragh axes: respect and`,
+// 	];
+// 	try {
+// 		for (const element of text) {
+// 			console.log(element);
+// 			const generated = await PlayHT.generate(element);
+
+// 			// Grab the generated file URL
+// 			const { audioUrl } = generated;
+
+// 			console.log("The url for the audio file is", audioUrl);
+// 		}
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// })();
 
 const app = express();
 
